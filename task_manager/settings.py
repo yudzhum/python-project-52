0cc8f982536720a5375ffb5c0163079a3cc6c5c3
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-07-06 22:42:13 +0300

    prepare for deployment on render

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -30,8 +30,11 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', False)
 
-ALLOWED_HOSTS = ["127.0.0.1", os.getenv('ALLOWED_HOST')]
+ALLOWED_HOSTS = ["127.0.0.1"]
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
