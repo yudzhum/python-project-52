4770d5c1c8893c24ef45f10426c1966aaa165910
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-07-31 21:35:39 +0300

    add test for login and logout

---
+++ task_manager/tests.py
@@ -0,0 +1,42 @@
+from django.test import TestCase
+from django.urls import reverse
+from django.core.exceptions import ObjectDoesNotExist
+from django.utils.translation import gettext_lazy as _
+
+from task_manager.utils import get_test_data
+from task_manager.users.models import CustomUser
+
+
+class IndexTest(TestCase):
+    fixtures = ['users.json']
+
+    def setUp(self):
+        self.user = CustomUser.objects.get(pk=1)
+        self.login_data = {
+            'username': self.user.username,
+            'password': self.user.password
+        }
+
+        self.login_user = CustomUser.objects.get(pk=2)
+    
+    def test_index_page(self):
+        response = self.client.get(reverse('home'))
+        self.assertEqual(response.status_code, 200)  
+
+    def test_login_page(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        response = self.client.get(reverse('login'))
+        self.assertEqual(response.status_code, 200)
+
+        response = self.client.post(reverse('login'), self.login_data, follow=True)
+        self.assertTrue(self.user.is_authenticated)
+
+
+    def test_logout(self):
+        self.client.force_login(self.login_user)
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('home'))
