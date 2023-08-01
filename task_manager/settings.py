c1a04f920896f30a70483b73cc6caaacefc11c4f
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-08-02 00:09:58 +0300

    update settings

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -171,6 +171,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 ROLLBAR = {
     'access_token': os.getenv('POST_SERVER_ITEM_ACCESS_TOKEN'),
     'environment': 'development' if DEBUG else 'production',
-    'branch': 'master',
+    'code_version': '1.0',
     'root': BASE_DIR,
 }
