e7b6a60df70cc3fc6744cf13f4bb46c9e7798771
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-08-03 00:34:55 +0300

    fix name display

--- task_manager/users/models.py
+++ task_manager/users/models.py
@@ -3,5 +3,6 @@ from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
     """User model"""
+    
     def __str__(self):
-        return self.username
+        return self.get_full_name()
