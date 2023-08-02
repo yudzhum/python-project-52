ec6a1a56dcf110f892bd29b1ab6c1dbff88ce904
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-08-02 20:13:51 +0300

    switch language

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -139,7 +139,12 @@ MESSAGE_TAGS = {
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
+
+LANGUAGES = (
+    ('en-us', 'English'),
+    ('ru-ru', 'Russian'),
+)
 
 TIME_ZONE = 'UTC'
 
