37c3e3fb5cae14953fb826d2519767e3656ecc4f
Yuliya Dzhumanazarova <yaschick451@gmail.com>
2023-08-02 20:02:06 +0300

    add webserver to allowed_host

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -36,6 +36,7 @@ RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 else:
+    ALLOWED_HOSTS.append('webserver')
     ALLOWED_HOSTS.append('127.0.0.1')
 
 # Application definition
